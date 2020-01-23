f76bd135aa0bf5705568e68bc9d4bdbb9fce9159
Sander Furre <sanderfu@stud.ntnu.no>
2020-01-23 16:58:20 +0100

    Added rudimentary tcp network functionality

---
+++ networking/tcp/main.go
@@ -0,0 +1,148 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"strings"
+)
+
+type ClientManager struct {
+	clients    map[*Client]bool
+	broadcast  chan []byte
+	register   chan *Client
+	unregister chan *Client
+}
+
+type Client struct {
+	socket net.Conn
+	data   chan []byte
+}
+
+func (manager *ClientManager) start() {
+	for {
+		select {
+		case connection := <-manager.register:
+			manager.clients[connection] = true
+			fmt.Println("Added new connection!")
+		case connection := <-manager.unregister:
+			if _, ok := manager.clients[connection]; ok {
+				close(connection.data)
+				delete(manager.clients, connection)
+				fmt.Println("A connection has terminated!")
+			}
+		case message := <-manager.broadcast:
+			for connection := range manager.clients {
+				select {
+				case connection.data <- message:
+				default:
+					close(connection.data)
+					delete(manager.clients, connection)
+				}
+			}
+		}
+	}
+}
+
+func (manager *ClientManager) recieve(client *Client) {
+	for {
+		message := make([]byte, 4096)
+		length, err := client.socket.Read(message)
+		if err != nil {
+			manager.unregister <- client
+			client.socket.Close()
+			break
+		}
+		if length > 0 {
+			fmt.Println("RECIEVED: " + string(message))
+			manager.broadcast <- message
+		}
+	}
+}
+
+func (manager *ClientManager) send(client *Client) {
+	defer client.socket.Close()
+	for {
+		select {
+		case message, ok := <-client.data:
+			if !ok {
+				return
+			}
+			client.socket.Write(message)
+		}
+	}
+}
+
+func startServerMode() {
+	fmt.Println("Starting server...")
+	listener, error := net.Listen("tcp", ":8080")
+	if error != nil {
+		fmt.Println(error)
+	}
+	manager := ClientManager{
+		clients:    make(map[*Client]bool),
+		broadcast:  make(chan []byte),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+	}
+	go manager.start()
+	for {
+		connection, _ := listener.Accept()
+		if error != nil {
+			fmt.Println(error)
+		}
+		client := &Client{socket: connection, data: make(chan []byte)}
+		manager.register <- client
+		go manager.recieve(client)
+		go manager.send(client)
+	}
+}
+
+//Now we shift focus to the client side
+
+func (client *Client) recieve() {
+	for {
+		message := make([]byte, 4096)
+		length, err := client.socket.Read(message)
+		if err != nil {
+			client.socket.Close()
+			break
+		}
+		if length > 0 {
+			fmt.Println("RECIEVED: " + string(message))
+		}
+	}
+}
+
+func startClientMode() {
+	fmt.Println("Starting client...")
+	connection, error := net.Dial("tcp", "localhost:8080")
+	if error != nil {
+		fmt.Println(error)
+	}
+	client := &Client{socket: connection}
+	go client.recieve()
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		message, _ := reader.ReadString('\n')
+		if string(message) == "end\n" {
+			terminationMessage := "Client user has terminated the session"
+			connection.Write([]byte(strings.TrimRight(terminationMessage, "\n")))
+			fmt.Println("Terminating client")
+			break
+		}
+		connection.Write([]byte(strings.TrimRight(message, "\n")))
+	}
+}
+
+func main() {
+	flagMode := flag.String("mode", "server", "start in client or server mode")
+	flag.Parse()
+	if strings.ToLower(*flagMode) == "server" {
+		startServerMode()
+	} else {
+		startClientMode()
+	}
+}
